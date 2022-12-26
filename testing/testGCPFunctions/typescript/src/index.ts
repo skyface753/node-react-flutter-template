855c32184fbb73bbe8f5fbe51df46a958ef31795
Sebastian Jörz <sjoerz@skyface.de>
2022-12-26 22:19:53 +0100

    Deleted grpc node

--- testing/testGCPFunctions/typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
-
-export const helloWorld: HttpFunction = (req, res) => {
-  res.send('Hello, World');
-};
-
-export const helloWorld2: HttpFunction = (req, res) => {
-  res.send('Hello, World 2');
-};
